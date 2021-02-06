4b811bc1c4327bd7c233460775d2a485c71e6b94
Lorenzo C <lorenzoc25@g.ucla.edu>
2021-02-05 21:32:58 -0800

    modified LeaderBoard.jsx based on MaterialUI

---
+++ src/components/LeaderBoard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const PlayerList = [
+   { userID = 'Carey'},
+   { userID = 'Mark Li' },
+   { userID = 'Jeffery Yu'},
+   { userID = 'Howard Qu'},
+   { userID = 'Lorenzo Cao'},
+   
+
+]
+
+export default function LeaderBoard(prop) {
+
+  return (
+    <List dense className={classes.root}>
+      {nameList.map((name) => {
+        
+        return (
+          <ListItem key={value} button>
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar n°${value + 1}`}
+                src={`/static/images/avatar/${value + 1}.jpg`}
+              />
+            </ListItemAvatar>
+            <ListItemText  primary = {prop.userID + `${value + 1}`} />
+            <ListItemSecondaryAction>
+              
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
