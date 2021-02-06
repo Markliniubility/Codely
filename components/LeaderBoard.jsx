89311d25c971f4dc9315b8ec3870877439a856ba
Lorenzo C <lorenzoc25@g.ucla.edu>
2021-02-06 00:17:52 -0800

    implemented leaderBoard component

--- components/LeaderBoard.jsx
+++ components/LeaderBoard.jsx
@@ -2,48 +2,72 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-
 import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '80%',
-    backgroundColor: theme.palette.background.paper
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
   },
 }));
 
-const PlayerList = [
-   { userID = 'Shuming Xu'},
-   { userID = 'Mark Li' },
-   { userID = 'Jeffery Yu'},
-   { userID = 'Howard Qu'},
-   { userID = 'Lorenzo Cao'}
-]
 
 export default function LeaderBoard(prop) {
+  const classes = useStyles;
+  const PlayerList = [
+    { userID : 'Shuming Xu',
+    profileSrc: "../dist/" ,
+    numMatches: 10},
+    { userID : 'Mark Li',
+    profileSrc: "../dist/" ,
+    numMatches: 13 },
+    { userID : 'Jeffery Yu',
+    profileSrc: "../dist/" ,
+    numMatches: 1},
+    { userID : 'Howard Qu',
+    profileSrc: "../dist/" ,
+    numMatches: 7},
+    { userID : 'Lorenzo Cao',
+    profileSrc: "../dist/" ,
+    numMatches: 33}
+  ]
 
-  return (
-    <List dense className={classes.root}>
-      {nameList.map((name) => {
-        
-        return (
-          <ListItem key={value} button>
-            <ListItemAvatar>
-              <Avatar
-                alt={`Avatar n°${value + 1}`}
-                src={`/static/images/avatar/${value + 1}.jpg`}
+  return (<div>
+          {
+            PlayerList.map(person=>{
+              return <List className={classes.root}>
+              <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt={person.userID} src={person.profileSrc} />
+              </ListItemAvatar>
+              <ListItemText
+                primary= {person.userID}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={person.inline}
+                      color="textPrimary"
+                    >
+                      Number of Matches
+                    </Typography>
+                    {person.numMatches}
+                  </React.Fragment>
+                }
               />
-            </ListItemAvatar>
-            <ListItemText  primary = {prop.userID + rank + `${value + 1}`} />
-            <ListItemSecondaryAction>
-              
-            </ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
-    </List>
-  );
+            </ListItem>
+            
+            <Divider variant="inset" component="li" />
+            </List>
+            })
+          }
+          </div>);
 }
