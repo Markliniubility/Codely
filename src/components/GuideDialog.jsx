1b650b76353da453cabd079791471d97f21979a0
JeffreytheCoder <35394596+JeffreytheCoder@users.noreply.github.com>
2021-02-06 22:44:19 +0800

    Edit phrasing

--- src/components/GuideDialog.jsx
+++ src/components/GuideDialog.jsx
@@ -80,9 +80,9 @@ export default function CustomizedDialogs() {
           <Typography gutterBottom>
             <p><b>Welcome to codely: a site where coding becomes fun!</b></p>
           </Typography>
-          <Typography gutterBottom>Codely provides a place for competing. There are two main modes for you.</Typography>
-          <Typography gutterBottom> 1. Compete with strangers through Match. </Typography>
-          <Typography gutterBottom> 2. Compete with your friends via Friends. </Typography>
+          <Typography gutterBottom>Codely provides a place for coders to compete and learn while having fun with online users and your friends! You can either:</Typography>
+          <Typography gutterBottom> 1. Compete with matched users online </Typography>
+          <Typography gutterBottom> 2. Compete with your friends </Typography>
           <Typography gutterBottom> You will be ranked! The only way to climb is to win the matches!</Typography>
           <Typography gutterBottom> There are five divisions: Bronze, Silver, Gold, Diamond, and Master. Each divison has five stars.</Typography>
           <Typography gutterBottom> Everyone starts from zero in Bronze. To gain a star, simply win a match. And you are facing the penalty of losing one if you lose.</Typography>
