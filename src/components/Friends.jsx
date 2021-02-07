8795c2ec403dff948ba8957cc6e960fc986d8259
lorenzoc25 <lorenzocao25@gmail.com>
2021-02-06 18:53:07 -0800

    changed botton and add popup

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import Popover from '@material-ui/core/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,11 +24,36 @@ const useStyles = makeStyles((theme) => ({
 export default function Friends(prop) {
   const classes = useStyles;
   const FriendList = [
-    { userID: "Shuming Xu", profileSrc: "../dist/", rankingStat: "Diamond", profilePage :"#" },
-    { userID: "Mark Li", profileSrc: "../dist/", rankingStat: "Platinum",profilePage :"#" },
-    { userID: "Jeffery Yu", profileSrc: "../dist/", rankingStat: "Platinum",profilePage :"#" },
-    { userID: "Howard Qu", profileSrc: "../dist/", rankingStat: "Gold", profilePage :"#"},
-    { userID: "Lorenzo Cao", profileSrc: "../dist/", rankingStat: "Silver", profilePage :"#"}
+    {
+      userID: "Shuming Xu",
+      profileSrc: "../dist/",
+      rankingStat: "Diamond",
+      IDnum :"#12345"
+    },
+    {
+      userID: "Mark Li",
+      profileSrc: "../dist/",
+      rankingStat: "Platinum",
+      IDnum :"16712"
+    },
+    {
+      userID: "Jeffery Yu",
+      profileSrc: "../dist/",
+      rankingStat: "Platinum",
+      IDnum :"11235"
+    },
+    {
+      userID: "Howard Qu",
+      profileSrc: "../dist/",
+      rankingStat: "Gold",
+      IDnum :"13315"
+    },
+    {
+      userID: "Lorenzo Cao",
+      profileSrc: "../dist/",
+      rankingStat: "Silver",
+      IDnum :"280222"
+    }
   ];
 
   return (
@@ -41,17 +68,38 @@ export default function Friends(prop) {
               <ListItemText
                 primary={person.userID}
                 secondary={
-                  <React.Fragment>
-                    {person.rankingStat}
-                  </React.Fragment>
+                  <React.Fragment>{person.rankingStat}</React.Fragment>
                 }
               />
-
-              <Button variant="contained" color="primary" onlick = "location.href = '{person.profilePage}">
-                  View Profile
-            </Button>
+<PopupState variant="popover" popupId="demo-popup-popover">
+      {(popupState) => (
+        <div>
+          <Button
+                variant="contained"
+                color="primary"
+                {...bindTrigger(popupState)}
+              >
+                View ID
+              </Button>
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <Box p={2}>
+              <Typography>ID: {person.IDnum}</Typography>
+            </Box>
+          </Popover>
+        </div>
+      )}
+    </PopupState>
             </ListItem>
-
           </List>
         );
       })}
